d816c2f24ab25eae378b8b3fe1aba73209a97ac9
Winny <winny.chelangat2@student.moringaschool.com>
2023-09-20 22:07:47 +0300

    Completed lab

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -11,5 +11,17 @@ class GetPrograms:
     return response.content
 
 
-programs = GetPrograms().get_programs()
-print(programs)
+def program_school(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+        programs_list = []
+        programs = json.loads(self.get_programs())
+        for program in programs:
+            programs_list.append(program["agency"])
+
+        return programs_list
+      
+programs = GetPrograms()
+programs_schools = programs.program_school()
+
+for school in set(programs_schools):
+    print(school)
